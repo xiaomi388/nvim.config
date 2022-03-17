f5ea7b9f109ce03eec3f622b9e9336525e3b1cb5
xiaomi388 <xiaomi388@gmail.com>
2022-03-17 16:44:07 -0400

    save

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,4 +1,9 @@
 require'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = {'go', 'python'},
+
+  highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+  },
 }
