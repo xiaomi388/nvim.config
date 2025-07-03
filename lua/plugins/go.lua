ed43101289cfb32d32cbbb409eb3e1eecb84dc2b
Yufan Chen <yufanchen@google.com>
2025-07-03 09:09:21 -0700

    save

---
+++ lua/plugins/go.lua
@@ -0,0 +1,12 @@
+return {
+  "ray-x/go.nvim",
+  dependencies = { -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    -- lsp_keymaps = false,
+    -- other options
+  },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,3 @@
-
 return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
@@ -9,48 +8,49 @@ return {
   },
   config = function()
     -- Keymaps for diagnostics
-    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, { noremap=true, silent=true })
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { noremap=true, silent=true })
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { noremap=true, silent=true })
-    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, { noremap=true, silent=true })
+    vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { noremap = true, silent = true })
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { noremap = true, silent = true })
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { noremap = true, silent = true })
+    vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, { noremap = true, silent = true })
 
     local on_attach = function(client, bufnr)
-      local bufopts = { noremap=true, silent=true, buffer = bufnr }
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+      local bufopts = { noremap = true, silent = true, buffer = bufnr }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
 
       -- Auto-format go files on save
-      if client.name == "gopls" then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          buffer = bufnr,
-          command = "lua require('go.format').goimport()",
-        })
-      end
+      -- replaced by conform.lua
+      -- if client.name == "gopls" then
+      --   vim.api.nvim_create_autocmd("BufWritePre", {
+      --     buffer = bufnr,
+      --     command = "lua require('go.format').goimport()",
+      --   })
+      -- end
     end
 
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-    local lspconfig = require('lspconfig')
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local lspconfig = require("lspconfig")
 
     -- Setup servers based on current directory
     local cwd = vim.fn.getcwd()
     if string.find(cwd, "google3") then
-      local servers = { 'ciderlsp', 'analysislsp' }
+      local servers = { "ciderlsp", "analysislsp" }
       for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup {
+        lspconfig[lsp].setup({
           on_attach = on_attach,
           capabilities = capabilities,
-        }
+        })
       end
     else
-      local servers = { 'rust_analyzer', 'ts_ls', 'clangd', 'golangci_lint_ls', 'pyright' }
+      local servers = { "rust_analyzer", "ts_ls", "clangd", "golangci_lint_ls", "pyright" }
       for _, server_name in ipairs(servers) do
-        lspconfig[server_name].setup {
+        lspconfig[server_name].setup({
           on_attach = on_attach,
           capabilities = capabilities,
-        }
+        })
       end
 
-      lspconfig.gopls.setup {
+      lspconfig.gopls.setup({
         on_attach = on_attach,
         capabilities = capabilities,
         settings = {
@@ -70,16 +70,15 @@ return {
             },
           },
         },
-      }
+      })
 
-      lspconfig.beancount.setup {
+      lspconfig.beancount.setup({
         on_attach = on_attach,
         capabilities = capabilities,
         init_options = {
           journal_file = vim.fn.expand("~/Documents/Ledger/main.beancount"),
         },
-      }
+      })
     end
   end,
 }
-
